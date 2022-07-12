5885c2b30e6aef797a03cc81c4219e0d7e811e71
Madasu-17 <105354667+Madasu-17@users.noreply.github.com>
2022-07-12 09:23:11 +0530

    Exceptions updated
    Exceptions Updated

--- employee-service/src/main/java/com/application/execption/GlobalExceptionHandler.java
+++ employee-service/src/main/java/com/application/execption/GlobalExceptionHandler.java
@@ -2,31 +2,73 @@ package com.application.execption;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(EmployeeNotFound.class)
-	public ResponseEntity<Object> handleEmployeeNotFound(EmployeeNotFound ex, WebRequest request) {
+	public ResponseEntity<Object> handleEmployeeNotFound(EmployeeNotFound ex,HttpStatus status, WebRequest request) {
 
-		return new ResponseEntity<Object>(
-				new ApiErrors(ex.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
+		String message= ex.getMessage();
+		message=("Employee Not Found");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String message= ex.getMessage();
+		message=("Request Method is not Supported, Change the Method");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
+	}
 
-		return new ResponseEntity<Object>(
-				new ApiErrors(ex.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String message= ex.getMessage();
+		message=("Media Type Not Supported, Check the Media Type");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String message= ex.getMessage();
+		message=("Missing Path Variable");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String message= ex.getMessage();
+		message=("Handleing Type Miss match");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String message= ex.getMessage();
+		message=("HTTP Message is Not Readable");
+		ApiErrors apiErrors = new ApiErrors(message,status,LocalDateTime.now());
+		return ResponseEntity.status(status).body(apiErrors);
 	}
 
 }
